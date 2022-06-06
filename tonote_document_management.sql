d52d6bd5cbb60a217f9efe718e5880a539836259
Shafi Akinropo <sakinropo@gmail.com>
2022-06-06 15:36:47 +0100

    update

--- tonote_document_management.sql
+++ tonote_document_management.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Jun 03, 2022 at 12:05 PM
+-- Generation Time: Jun 06, 2022 at 04:33 PM
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 8.0.10
 
@@ -49,7 +49,8 @@ CREATE TABLE `admin` (
 --
 
 INSERT INTO `admin` (`id`, `first_name`, `last_name`, `email`, `profile_img`, `hashed_password`, `reset_password`, `admin_level`, `account_status`, `process_request`, `created_at`, `updated_at`, `created_by`, `deleted`) VALUES
-(1, 'Admin', 'One', 'doc@gettonote.com', '', '$2y$10$df9kgE/lDm/j3TyklJyoguf5w0MTfZE8tpJqs1EUINLn7XqZYptka', '', '1', NULL, '', '2022-05-20 20:03:07', '2022-05-20 22:02:23', NULL, '');
+(1, 'Admin', 'One', 'doc@gettonote.com', '', '$2y$10$df9kgE/lDm/j3TyklJyoguf5w0MTfZE8tpJqs1EUINLn7XqZYptka', '', '1', NULL, '', '2022-05-20 20:03:07', '2022-05-20 22:02:23', NULL, ''),
+(2, 'Shafi', 'Akinropo', 'sakinropo@gmail.com', '', '$2y$10$Mg.uyNtVJiNx20X8i6z5xePbcQKoXFhsET7i5Fedug6rfzgIzF0Y.', '0', '1', '1', '0', '2022-06-06 03:40:24', '2022-06-06 03:40:24', '1', '');
 
 -- --------------------------------------------------------
 
@@ -74,29 +75,8 @@ CREATE TABLE `document` (
 --
 
 INSERT INTO `document` (`id`, `document_id`, `title`, `filename`, `status`, `created_at`, `updated_at`, `created_by`, `deleted`) VALUES
-(1, 'Tonote-748', '', '62900cae901e8_2nd ECM 1443 updated.pdf', '1', '2022-05-27 00:26:38', '', '0', ''),
-(2, 'Tonote-748', '', '62900d9249ac9_2nd ECM 1443 updated.pdf', '1', '2022-05-27 00:30:26', '', '0', ''),
-(3, 'Tonote-748', '', '6294b131b552b_03_FGN Sukuk Subscription Form 2021 - RETAIL.pdf', '1', '2022-05-30 12:57:37', '', '0', ''),
-(4, 'ToNote-62', '', '6294b269ed037_6pics.jpeg', '1', '2022-05-30 13:02:49', '', '0', ''),
-(5, 'ToNote_6294b85ce433c', '', '6294b85ce434b_03_FGN Sukuk Subscription Form 2021 - RETAIL.pdf', '1', '2022-05-30 13:28:12', '', '0', ''),
-(6, 'ToNote_6295cc36a41d9', '', '6295cc36a41fe_03_FGN Sukuk Subscription Form 2021 - RETAIL.pdf', '1', '2022-05-31 09:05:10', '', '0', ''),
-(7, 'ToNote_6295ccac78e00', '', '6295ccac78e23_2nd ECM 1443 updated.pdf', '1', '2022-05-31 09:07:08', '', '0', ''),
-(8, 'ToNote_6295ccc140421', '', '6295ccc140432_2nd ECM 1443 updated.pdf', '1', '2022-05-31 09:07:29', '', '0', ''),
-(9, 'ToNote_6295ccf60bec1', '', '6295ccf60beda_{1317ffb0-607d-4ecd-bf36-c7affd11a79a}_Forrester_State_of_Systems_of_Agreement_2021.pdf', '1', '2022-05-31 09:08:22', '', '0', ''),
-(10, 'ToNote_6295cd413e6c4', '', '6295cd413e6cf_03_FGN Sukuk Subscription Form 2021 - RETAIL.pdf', '1', '2022-05-31 09:09:37', '', '0', ''),
-(11, 'ToNote_6295cd536d7af', '', '6295cd536d7bd_2nd ECM 1443 updated.pdf', '1', '2022-05-31 09:09:55', '', '0', ''),
-(12, 'ToNote_6295cd5b20cdd', '', '6295cd5b20cf2_{1317ffb0-607d-4ecd-bf36-c7affd11a79a}_Forrester_State_of_Systems_of_Agreement_2021.pdf', '1', '2022-05-31 09:10:03', '', '0', ''),
-(13, 'ToNote_6296f7e8ca95d', 'Contract', '6296f7e8ca968_2nd ECM 1443 updated.pdf', '1', '2022-06-01 06:23:52', '', '0', ''),
-(14, 'ToNote_6296f81e786f4', 'Contract', '6296f81e78704_2nd ECM 1443 updated.pdf', '1', '2022-06-01 06:24:46', '', '0', ''),
-(15, 'ToNote_6296f86d56535', 'Contract', '6296f86d56544_2nd ECM 1443 updated.pdf', '1', '2022-06-01 06:26:05', '', '0', ''),
-(16, 'ToNote_6296f94b09890', 'Contarct', '6296f94b0989c_2nd ECM 1443 updated.pdf', '1', '2022-06-01 06:29:47', '', '0', ''),
-(17, 'ToNote_6296f95ce8593', 'Contarct', '6296f95ce859f_2nd ECM 1443 updated.pdf', '1', '2022-06-01 06:30:04', '', '0', ''),
-(18, 'ToNote_6296f96ab0764', 'Contarct', '6296f96ab078f_2nd ECM 1443 updated.pdf', '1', '2022-06-01 06:30:18', '', '0', ''),
-(19, 'ToNote_6296f9e7ebcd6', 'contarct', '6296f9e7ebce4_2nd ECM 1443 updated.pdf', '1', '2022-06-01 06:32:23', '', '0', ''),
-(20, 'ToNote_6296fb5d7402d', 'Not Set', '6296fb5d74038_2nd ECM 1443 updated.pdf', '1', '2022-06-01 06:38:37', '', '0', ''),
-(21, 'ToNote_6296ff264689c', 'Not Set', '6296ff26468b4_2nd ECM 1443 updated.pdf', '1', '2022-06-01 06:54:46', '', '0', ''),
-(22, 'ToNote_629701b85071d', 'Not Set', '629701b850728_2nd ECM 1443 updated.pdf', '1', '2022-06-01 07:05:44', '', '0', ''),
-(23, 'ToNote_629702074db39', 'Contarct', '629702074db44_03_FGN Sukuk Subscription Form 2021 - RETAIL.pdf', '1', '2022-06-01 07:07:03', '', '0', '');
+(1, 'ToNote_629cd7a54dfcf', 'Contract Agreement', '629cd7a54dfda_updated.pdf', '1', '2022-06-05 17:19:49', '', '1', ''),
+(2, 'ToNote_629dd8d7cac72', 'Scholarship', '629dd8d7cac7c_CB SCHOLARSHIP APPLICATION  2022.pdf', '1', '2022-06-06 11:37:11', '', '2', '');
 
 -- --------------------------------------------------------
 
@@ -105,6 +85,33 @@ INSERT INTO `document` (`id`, `document_id`, `title`, `filename`, `status`, `cre
 --
 
 CREATE TABLE `documentImage` (
+  `id` int(11) NOT NULL,
+  `document_id` varchar(50) NOT NULL,
+  `title` varchar(100) NOT NULL,
+  `status` varchar(50) NOT NULL,
+  `created_at` varchar(50) NOT NULL,
+  `updated_at` varchar(50) NOT NULL,
+  `created_by` varchar(50) NOT NULL,
+  `deleted` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `documentImage`
+--
+
+INSERT INTO `documentImage` (`id`, `document_id`, `title`, `status`, `created_at`, `updated_at`, `created_by`, `deleted`) VALUES
+(1, '629cdef830f3f', 'Contract Agreement', '2', '2022-06-05 17:51:12', '', '1', ''),
+(2, '629d6ac27b65b', 'Affidavit of Sponsorship - Education', '2', '2022-06-06 03:47:38', '', '2', ''),
+(3, '629dd92806f4e', 'Scholarship', '2', '2022-06-06 11:38:47', '', '2', ''),
+(4, '629de01511020', 'Affidavit of Marriage', '2', '2022-06-06 12:08:12', '', '2', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `documentImageDetails`
+--
+
+CREATE TABLE `documentImageDetails` (
   `id` int(11) NOT NULL,
   `document_id` varchar(50) NOT NULL,
   `title` varchar(100) NOT NULL,
@@ -117,28 +124,75 @@ CREATE TABLE `documentImage` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `documentImage`
+-- Dumping data for table `documentImageDetails`
 --
 
-INSERT INTO `documentImage` (`id`, `document_id`, `title`, `filename`, `status`, `created_at`, `updated_at`, `created_by`, `deleted`) VALUES
-(1, '6294dc9bd4fa8', 'Sworn Affidavit for  bacherlorhood', '6294dc9bd4f9dimg.png', '2', '2022-05-30 16:02:51', '', '1', ''),
-(2, '6294dc9c02bcf', 'Sworn Affidavit for  bacherlorhood', '6294dc9c02bbfimg.png', '2', '2022-05-30 16:02:52', '', '1', ''),
-(3, '6294df94c0f08', 'Sworn Affidavit for  bacherlorhood', '6294dfab64a46img.png', '2', '2022-05-30 16:15:55', '', '1', ''),
-(4, '6294df94c0f08', 'Sworn Affidavit for  bacherlorhood', '6294dfab7c160img.png', '2', '2022-05-30 16:15:55', '', '1', ''),
-(5, '6294df94c0f08', 'Sworn Affidavit for  bacherlorhood', '6294e01609fcaimg.png', '2', '2022-05-30 16:17:42', '', '1', ''),
-(6, '6294df94c0f08', 'Sworn Affidavit for  bacherlorhood', '6294e0161cddeimg.png', '2', '2022-05-30 16:17:42', '', '1', ''),
-(7, '6294df94c0f08', 'Sworn Affidavit for  bacherlorhood', '6294e03d0fd59img.png', '2', '2022-05-30 16:18:21', '', '1', ''),
-(8, '6294df94c0f08', 'Sworn Affidavit for  bacherlorhood', '6294e03d1ead7img.png', '2', '2022-05-30 16:18:21', '', '1', ''),
-(9, '6294e0438a27b', 'Sworn Affidavit for  bacherlorhood', '6294e04b5d405img.png', '2', '2022-05-30 16:18:35', '', '1', ''),
-(10, '6294e0438a27b', 'Sworn Affidavit for  bacherlorhood', '6294e04b6b984img.png', '2', '2022-05-30 16:18:35', '', '1', ''),
-(11, '6294e1794011b', 'Sworn Affidavit for  bacherlorhood', '6294e1855ead8img.png', '2', '2022-05-30 16:23:49', '', '1', ''),
-(12, '6294e1794011b', 'Sworn Affidavit for  bacherlorhood', '6294e18590023img.png', '2', '2022-05-30 16:23:49', '', '1', ''),
-(13, '6295b0955459c', 'Sworn Affidavit for  bacherlorhood', '6295b117b5b8eimg.png', '2', '2022-05-31 07:09:27', '', '1', ''),
-(14, '629651f064d20', '', '62965223bf99bimg.png', '2', '2022-05-31 18:36:35', '', '0', ''),
-(15, '62965542d422e', '', '6296554add114img.png', '2', '2022-05-31 18:50:02', '', '0', ''),
-(16, '6297020a8327c', '', '629703f0ac630img.png', '2', '2022-06-01 07:15:12', '', '0', ''),
-(17, '6297020a8327c', '', '629703f0c664cimg.png', '2', '2022-06-01 07:15:12', '', '0', ''),
-(18, '6297ad6d8758a', '', '6297ad75e57f6img.png', '2', '2022-06-01 19:18:29', '', '0', '');
+INSERT INTO `documentImageDetails` (`id`, `document_id`, `title`, `filename`, `status`, `created_at`, `updated_at`, `created_by`, `deleted`) VALUES
+(1, '629cdef830f3f', '', '629cdf00c0e07img.png', '2', '2022-06-05 17:51:12', '', '1', ''),
+(2, '629cdef830f3f', '', '629cdf00c7ebbimg.png', '2', '2022-06-05 17:51:12', '', '1', ''),
+(3, '629cdef830f3f', '', '629cdf00ccf7aimg.png', '2', '2022-06-05 17:51:12', '', '1', ''),
+(4, '629cdef830f3f', '', '629cdf00d1b09img.png', '2', '2022-06-05 17:51:12', '', '1', ''),
+(5, '629cdef830f3f', '', '629cdf00d6c5cimg.png', '2', '2022-06-05 17:51:12', '', '1', ''),
+(6, '629cdef830f3f', '', '629cdf00dac89img.png', '2', '2022-06-05 17:51:12', '', '1', ''),
+(7, '629cdef830f3f', '', '629cdf0102f41img.png', '2', '2022-06-05 17:51:13', '', '1', ''),
+(8, '629d6ac27b65b', '', '629d6aca36e4bimg.png', '2', '2022-06-06 03:47:38', '', '2', ''),
+(9, '629dd92806f4e', '', '629dd93824392img.png', '2', '2022-06-06 11:38:48', '', '2', ''),
+(10, '629de01511020', '', '629de01cd18b1img.png', '2', '2022-06-06 12:08:12', '', '2', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `documentResource`
+--
+
+CREATE TABLE `documentResource` (
+  `id` int(11) NOT NULL,
+  `document_id` varchar(50) NOT NULL,
+  `filename` varchar(50) NOT NULL,
+  `tool_id` varchar(50) NOT NULL,
+  `tool_type` varchar(50) NOT NULL,
+  `tool_name` varchar(100) NOT NULL,
+  `tool_class` varchar(50) NOT NULL,
+  `tool_pos_top` varchar(50) NOT NULL,
+  `tool_pos_left` varchar(50) NOT NULL,
+  `created_at` varchar(50) NOT NULL,
+  `updated_at` varchar(50) NOT NULL,
+  `created_by` varchar(50) NOT NULL,
+  `deleted` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `documentResource`
+--
+
+INSERT INTO `documentResource` (`id`, `document_id`, `filename`, `tool_id`, `tool_type`, `tool_name`, `tool_class`, `tool_pos_top`, `tool_pos_left`, `created_at`, `updated_at`, `created_by`, `deleted`) VALUES
+(54, '629dd92806f4e', 'sign1654512116img.png', '26', '2', 'Sign', 'tool-box tool-style main-element', '1028.5', '394', '2022-06-06 11:41:52', '2022-06-06 11:43:54', '', ''),
+(57, '629dd92806f4e', 'initial1654512116img.png', '12', '2', 'Initial', 'tool-box tool-style main-element', '1084.5', '581', '2022-06-06 11:46:58', '2022-06-06 11:47:17', '', ''),
+(58, '629dd92806f4e', '', '78', '1', 'Textarea', 'tool-box tool-style main-element', '732.5', '531', '2022-06-06 11:48:24', '', '', ''),
+(59, '629dd92806f4e', '', '17', '1', 'Date', 'tool-box tool-style main-element', '869.5', '346', '2022-06-06 11:50:29', '', '', ''),
+(60, '629dd92806f4e', '', '13', '1', 'Seal', 'tool-box tool-style main-element', '907.5', '617', '2022-06-06 11:50:30', '', '', ''),
+(61, '629dd92806f4e', '', '24', '1', 'Stamp', 'tool-box tool-style main-element', '976.5', '498', '2022-06-06 11:50:33', '', '', ''),
+(63, '629de01511020', 'sign1654512116img.png', '62', '2', 'Sign', 'tool-box tool-style main-element', '908.5', '386.3359375', '2022-06-06 14:10:19', '2022-06-06 14:10:25', '', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `participants`
+--
+
+CREATE TABLE `participants` (
+  `id` int(11) NOT NULL,
+  `document_id` varchar(50) NOT NULL,
+  `first_name` varchar(100) NOT NULL,
+  `last_name` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `phone` varchar(50) NOT NULL,
+  `role` varchar(50) NOT NULL,
+  `created_at` varchar(50) NOT NULL,
+  `updated_at` varchar(50) NOT NULL,
+  `created_by` varchar(50) NOT NULL,
+  `deleted` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -168,6 +222,7 @@ CREATE TABLE `signatureDetails` (
   `filename` varchar(50) NOT NULL,
   `type` varchar(100) NOT NULL,
   `category` varchar(100) NOT NULL,
+  `signature_id` varchar(50) NOT NULL,
   `created_at` varchar(100) NOT NULL,
   `updated_at` varchar(100) NOT NULL,
   `created_by` varchar(50) NOT NULL,
@@ -178,9 +233,11 @@ CREATE TABLE `signatureDetails` (
 -- Dumping data for table `signatureDetails`
 --
 
-INSERT INTO `signatureDetails` (`id`, `user_id`, `filename`, `type`, `category`, `created_at`, `updated_at`, `created_by`, `deleted`) VALUES
-(1, '0', 'sign1654145135img.png', '1', '1', '2022-06-02 21:39:59', '2022-06-02 21:39:59', '0', ''),
-(2, '0', 'initial1654145136img.png', '1', '2', '2022-06-02 21:39:59', '2022-06-02 21:39:59', '0', '');
+INSERT INTO `signatureDetails` (`id`, `user_id`, `filename`, `type`, `category`, `signature_id`, `created_at`, `updated_at`, `created_by`, `deleted`) VALUES
+(1, '1', 'sign1654145135img.png', '1', '1', '1', '2022-06-02 21:39:59', '2022-06-02 21:39:59', '0', ''),
+(2, '1', 'initial1654145136img.png', '1', '2', '1', '2022-06-02 21:39:59', '2022-06-02 21:39:59', '0', ''),
+(38, '2', 'sign1654512116img.png', '1', '1', '', '2022-06-06 11:41:56', '2022-06-06 11:41:56', '2', ''),
+(39, '2', 'initial1654512116img.png', '1', '2', '', '2022-06-06 11:41:56', '2022-06-06 11:41:56', '2', '');
 
 -- --------------------------------------------------------
 
@@ -248,6 +305,24 @@ ALTER TABLE `documentImage`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `documentImageDetails`
+--
+ALTER TABLE `documentImageDetails`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `documentResource`
+--
+ALTER TABLE `documentResource`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `participants`
+--
+ALTER TABLE `participants`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `signature`
 --
 ALTER TABLE `signature`
@@ -273,19 +348,37 @@ ALTER TABLE `templates`
 -- AUTO_INCREMENT for table `admin`
 --
 ALTER TABLE `admin`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `document`
 --
 ALTER TABLE `document`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `documentImage`
 --
 ALTER TABLE `documentImage`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `documentImageDetails`
+--
+ALTER TABLE `documentImageDetails`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `documentResource`
+--
+ALTER TABLE `documentResource`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=66;
+
+--
+-- AUTO_INCREMENT for table `participants`
+--
+ALTER TABLE `participants`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `signature`
@@ -297,7 +390,7 @@ ALTER TABLE `signature`
 -- AUTO_INCREMENT for table `signatureDetails`
 --
 ALTER TABLE `signatureDetails`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
 
 --
 -- AUTO_INCREMENT for table `templates`
